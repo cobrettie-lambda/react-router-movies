d8196566dd8c8e522f53dd8758e886481e150cfd
Cobrettie <cobrettiegarner3@gmail.com>
2019-06-22 22:56:31 -0400

    update Movie.js comment regarding componentDidMount

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,6 +12,7 @@ export default class Movie extends Component {
   componentDidMount() {
     // change this line to grab the id passed on the URL
     // const id = 1;
+    //
     this.fetchMovie(this.props.match.params.id);
   }
 
